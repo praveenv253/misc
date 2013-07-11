5191905abd39f5fc84a27cb0613f6e3ef8ed4e01
Praveen Venkatesh <praveenv253@gmail.com>
2013-07-11 21:45:32 +0530

    Add wrapper for the log program.
    - log.sh commits every log message and pushes it to a remote.
    - This makes for an effective automatic backup mechanism.

---
+++ log.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This program is a thin wrapper to provide git commit support to the logging
+# program.
+
+LOGBINARY="$HOME/C++Files/log"
+LOGDIR="$HOME/Documents/.logs"
+
+$LOGBINARY $@
+
+cd $LOGDIR
+
+# Should already be a git repo with logfile added.
+git commit -a -q -m "New log message."
+# The repo should already be configured with a remote.
+git push -q &
+
+# Reset the working directory quietly.
+cd - > /dev/null
