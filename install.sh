afbcba04d7b1fb54ee3a572db6157b23b9e1ebdd
Praveen Venkatesh <praveenv253@gmail.com>
2014-07-13 17:16:33 +0530

    Add shortcut creation to install.sh.

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# misc repo path
+MISC_REPO=$(pwd)
+
 # Install packages from reinstall
-REINSTALL_FILE="reinstall"
+REINSTALL_FILE="$MISC_REPO/reinstall"
 PACKAGES=$(cat $REINSTALL_FILE | sed 's/[#%].*$//' | sed 's/^\s.*//')
 sudo apt-get install $PACKAGES
 
@@ -19,3 +22,18 @@ git submodule update
 PATCHED_FONT_FILE="DejaVuSansMono-Powerline.ttf"
 mkdir "$HOME/.fonts"
 cp "$HOME/.vim/$PATCHED_FONT_FILE" "$HOME/.fonts/"
+
+# Make shortcuts
+cd "$HOME"
+ln -s "$MISC_REPO/bashrc" .bashrc
+ln -s "$MISC_REPO/bash_aliases" .bash_aliases
+ln -s "$MISC_REPO/inputrc" .inputrc
+ln -s "$MISC_REPO/gitconfig" .gitconfig
+ln -s "$MISC_REPO/gitignore_global" .gitignore_global
+mkdir "$HOME/bin"
+cd "$HOME/bin"
+ln -s "$MISC_REPO/tasks.py" tasks
+ln -s "$MISC_REPO/tvim.sh" tvim
+mkdir -p "$HOME/.local/share/applications"
+cd "$HOME/.local/share/applications"
+ln -s "$MISC_REPO/tvim.desktop"
