fef1bbfd1e5c0e85df90539ed4ecf049e1326b7d
Praveen Venkatesh <praveenv253@gmail.com>
2014-07-13 17:14:47 +0530

    Add gre-words and pick.py.

---
+++ pick.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from random import choice
+
+WORD_FILE = 'gre-words'
+
+f = open(WORD_FILE)
+content = f.read()
+f.close()
+
+lines = content.split('\n')
+
+while(1):
+    try:
+        random_line = choice(lines)
+    except IndexError:
+        break
+    lines.remove(random_line)
+    try:
+        first_letter = list(random_line)[0]
+    except IndexError:
+        continue
+    if first_letter == ' ' or first_letter == '#':
+        continue
+    word = random_line.split('-')[0].strip()
+    print word,
+    try:
+        char = raw_input()
+    except EOFError:
+        break
+    else:
+        if char != '':
+            try:
+                print random_line.split('-', 1)[1]
+            except IndexError:
+                print 'None'
+        continue
+
