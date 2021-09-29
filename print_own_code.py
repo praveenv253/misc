cb279e08ce60d38607357d826dd82f7020e5916f
Praveen Venkatesh <praveenv253@gmail.com>
2021-09-29 11:33:37 -0700

    Add script that prints its own code as output.

---
+++ print_own_code.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+output = '#!/usr/bin/env python3{3}{3}output = {1}{0}{1}; print(output.format(output, {1}{2}{1}{1}, {1}{2}{2}{1}, {1}{2}n{1}))'; print(output.format(output, '\'', '\\', '\n'))
